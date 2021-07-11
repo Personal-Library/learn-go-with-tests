1075d3a62815fcade40ec40ccefd4edacff6ee28
Anthony Bui <nhut.anthony.bui@gmail.com>
2021-07-10 23:25:48 -0500

    feat: Context package completed

--- context/context_test.go
+++ context/context_test.go
@@ -8,43 +8,6 @@ import (
 	"time"
 )
 
-type StubStore struct {
-	response string
-}
-
-func (s *StubStore) Fetch() string {
-	return s.response
-}
-
-type SpyStore struct {
-	response  string
-	cancelled bool
-	t         *testing.T
-}
-
-func (s *SpyStore) assertWasCancelled() {
-	s.t.Helper()
-	if !s.cancelled {
-		s.t.Error("store was not told to cancel")
-	}
-}
-
-func (s *SpyStore) assertWasNotCancelled() {
-	s.t.Helper()
-	if s.cancelled {
-		s.t.Error("store was told to cancel")
-	}
-}
-
-func (s *SpyStore) Fetch() string {
-	time.Sleep(100 * time.Millisecond)
-	return s.response
-}
-
-func (s *SpyStore) Cancel() {
-	s.cancelled = true
-}
-
 func TestServer(t *testing.T) {
 	data := "hello, world"
 
@@ -60,8 +23,6 @@ func TestServer(t *testing.T) {
 		if response.Body.String() != data {
 			t.Errorf("got %s want %s", response.Body.String(), data)
 		}
-
-		store.assertWasNotCancelled()
 	})
 
 	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
@@ -74,10 +35,11 @@ func TestServer(t *testing.T) {
 		time.AfterFunc(5*time.Millisecond, cancel)
 		request = request.WithContext(cancellingCtx)
 
-		response := httptest.NewRecorder()
-
+		response := &SpyResponseWriter{}
 		svr.ServeHTTP(response, request)
 
-		store.assertWasCancelled()
+		if response.written {
+			t.Error("a response should not have been written")
+		}
 	})
 }
